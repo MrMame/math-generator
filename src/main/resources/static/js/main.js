b005384946e25ad18ed836df41565e36a0fb3139
MrMame <mm@mail.de>
2022-10-27 21:47:09 +0200

    Using form event bubbling to start form inputs validation.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,22 +3,8 @@ import {validateForm} from "./main-page-validator.js";
 
 
 function addEventHandlers(){
-    /* Form Validating */
-    var sections = document.querySelectorAll('input[type=number]');
-    for(var i = 0;i < sections.length; i++){
-        sections[i].addEventListener('change',validateForm);
-    }
-
-    /* Checkboxes */
-    var sections = document.querySelectorAll('input[type=checkbox]');
-        for(var i = 0;i < sections.length; i++){
-            sections[i].addEventListener('change',validateForm);
-        }
-
-    /* Test Button */
-    const submitButton = document.querySelector('button[id=validateButton]');
-    submitButton.addEventListener('click',validateForm );
-
+    var theForm = document.querySelector('form');
+    theForm.addEventListener('change',validateForm);
 }
 
 
