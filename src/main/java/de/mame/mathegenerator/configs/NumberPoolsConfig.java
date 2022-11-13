0ac847d41f3d6f152c9f8546e1d27f3239dff0a7
MrMame <mm@mail.de>
2022-11-13 22:43:30 +0100

    Placed RandomNumberPools into own config

--- src/main/java/de/mame/mathegenerator/configs/NumberPoolsConfig.java
+++ src/main/java/de/mame/mathegenerator/configs/NumberPoolsConfig.java
@@ -1,23 +1,20 @@
 package de.mame.mathegenerator.configs;
 
-import de.mame.mathegenerator.logics.mathGenerators.AdditionMathGenerator;
 import de.mame.mathegenerator.logics.numberPools.RandomNumberPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class MathGeneratorsConfig {
-
-
+public class NumberPoolsConfig {
 
     @Bean
     @Primary
     @Autowired
-    AdditionMathGenerator getAdditionMathGenerator(RandomNumberPool theRandomNumberPool){
-        return new AdditionMathGenerator(theRandomNumberPool);
+    public RandomNumberPool getRandomNumberPool() {
+        return new RandomNumberPool();
     }
+
 
 }
