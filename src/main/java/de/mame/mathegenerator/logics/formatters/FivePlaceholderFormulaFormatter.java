af0c560f02faf38a844c22221c0857697526e998
MrMame <mm@mail.de>
2022-11-14 21:24:48 +0100

    Created class

---
+++ src/main/java/de/mame/mathegenerator/logics/formatters/FivePlaceholderFormulaFormatter.java
@@ -0,0 +1,37 @@
+package de.mame.mathegenerator.logics.formatters;
+
+import de.mame.mathegenerator.model.formulas.Formula;
+import de.mame.mathegenerator.model.formulas.formulaMembers.FormulaMember;
+import de.mame.mathegenerator.model.formulas.formulaMembers.mathOperators.MathOperator;
+import de.mame.mathegenerator.model.formulas.formulaMembers.numbers.RealNumber;
+
+public class FivePlaceholderFormulaFormatter implements FormulaFormatter{
+
+    private Formula _theFormula;
+    private static String FORMAT_STRING_NUMBER = "%5d";
+    private static String FORMAT_STRING_OPERATOR = "%5s";
+
+
+    public FivePlaceholderFormulaFormatter(Formula Formula) {
+        this._theFormula = Formula;
+    }
+
+    @Override
+    public String getFormattedString() {
+        StringBuilder theFormulaString = new StringBuilder();
+        String formattedMember="";
+        // Format Each Member of the Formula and add it to the return string
+        for(FormulaMember member:this._theFormula.GetFormulaMembers()){
+            // Each Type of Formulamember has its own format string
+            if(member.getClass() == Number.class){
+                formattedMember = String.format(this.FORMAT_STRING_NUMBER, ((RealNumber) member).GetIntegerValue());
+            }else if(member.getClass() == MathOperator.class){
+                formattedMember = String.format(this.FORMAT_STRING_OPERATOR, ((MathOperator) member).GetCharacter());
+            }
+            // add to return string
+            theFormulaString.append(formattedMember);
+        }
+        // Return the formatted string
+        return theFormulaString.toString();
+    }
+}
