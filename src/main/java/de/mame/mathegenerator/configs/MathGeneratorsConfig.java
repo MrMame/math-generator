083e6073e8d4d85f2dc2725e9ba1306d325af01e
MrMame <mm@mail.de>
2022-11-14 20:11:27 +0100

    Added Randomizer Interface Parameter for constructor injection

--- src/main/java/de/mame/mathegenerator/configs/MathGeneratorsConfig.java
+++ src/main/java/de/mame/mathegenerator/configs/MathGeneratorsConfig.java
@@ -2,11 +2,12 @@ package de.mame.mathegenerator.configs;
 
 import de.mame.mathegenerator.logics.mathGenerators.AdditionMathGenerator;
 import de.mame.mathegenerator.logics.numberPools.RandomNumberPool;
+import de.mame.mathegenerator.logics.randomizers.Randomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
+
 
 @Configuration
 public class MathGeneratorsConfig {
@@ -16,8 +17,8 @@ public class MathGeneratorsConfig {
     @Bean
     @Primary
     @Autowired
-    AdditionMathGenerator getAdditionMathGenerator(RandomNumberPool theRandomNumberPool){
-        return new AdditionMathGenerator(theRandomNumberPool);
+    AdditionMathGenerator getAdditionMathGenerator(RandomNumberPool theRandomNumberPool, Randomizer theRandomizer){
+        return new AdditionMathGenerator(theRandomNumberPool,theRandomizer);
     }
 
 }
